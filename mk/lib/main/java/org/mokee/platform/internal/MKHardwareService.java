20b007c60fde1d89f408bffea85926bdf7a5198c
Steve Kondik <steve@cyngn.com>
2015-10-03 00:00:02 -0700

    mksdk: Add helper class for working with colors
     * A bunch of generally useful algorithms and helpers for performing
       colorspace conversions and other color-related math.
    
    Change-Id: If54ca156fc5473f6c1b39eb53bd871f48c3dc2a1

--- mk/lib/main/java/org/mokee/platform/internal/MKHardwareService.java
+++ mk/lib/main/java/org/mokee/platform/internal/MKHardwareService.java
@@ -535,12 +535,12 @@ public class MKHardwareService extends SystemService implements ThermalUpdateCal
         @Override
         public boolean isSunlightEnhancementSelfManaged() {
             mContext.enforceCallingOrSelfPermission(
-                    cyanogenmod.platform.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS, null);
-            if (!isSupported(CMHardwareManager.FEATURE_SUNLIGHT_ENHANCEMENT)) {
+                    mokee.platform.Manifest.permission.HARDWARE_ABSTRACTION_ACCESS, null);
+            if (!isSupported(MKHardwareManager.FEATURE_SUNLIGHT_ENHANCEMENT)) {
                 Log.e(TAG, "Sunlight enhancement is not supported");
                 return false;
             }
-            return mCmHwImpl.isSunlightEnhancementSelfManaged();
+            return mMkHwImpl.isSunlightEnhancementSelfManaged();
         }
 
         @Override
