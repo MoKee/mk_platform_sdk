8a3debdbb3378b6d07dc6c831a86253ca86f7774
Roman Birg <roman@cyngn.com>
2015-10-28 17:51:14 -0700

    CMSettings: add privacy guard default key
    Change-Id: Iba10fcf3978e308f1b25a8d13570c420f7589a1b
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/java/mokee/providers/MKSettings.java
+++ src/java/mokee/providers/MKSettings.java
@@ -1073,6 +1073,12 @@ public final class MKSettings {
          */
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
+        /**
+         * Whether newly installed apps should run with privacy guard by default
+         * @hide
+         */
+        public static final String PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+
         // endregion
     }
 
