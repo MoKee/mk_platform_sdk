34a2fcd21632b0d403c8954e3f5c55d8edbb0669
d34d <clark@cyngn.com>
2015-12-21 10:34:15 -0800

    ExtView: Add keyguard specific interface for KeyguardExternalView
    KeyguardExternalView requires extra state callbacks that can be passed
    on to the KeyguardExternalViewProvider.  For this reason the
    KeyguardExternalViewProvider no longer extends ExternalViewProvider
    and instead provides the full implementation needed for ExternalViews
    as well as the specific keyguard components.
    
    Change-Id: Icf3589a201bf0ab29ba6e00e2bd6c0149e955e1d

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -43,3 +43,6 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/org.mo
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/org.mokee.platform.internal_intermediates)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/docs/mk-api-stubs-timestamp)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/mksdk_stubs_current_intermediates)
+# KeyguardExternalView uses a new interface which requires cleaning to avoid a runtime exception
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/org.mokee.platform_intermediates)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/org.mokee.platform.sdk_intermediates)
