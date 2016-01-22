5e93a4d3d7fa228fc0051023900db67e1bf52e99
Ricardo Cerqueira <ricardo@cyngn.com>
2016-01-22 18:07:34 +0000

    Add overlay for SWAP_VOLUME_KEYS_ON_ROTATION
    This got lost along the transition
    
    Change-Id: I35f5c6cdd73ff1d4a590fa6980d3214dda2bd740

--- packages/MKSettingsProvider/src/org/mokee/mksettings/MKDatabaseHelper.java
+++ packages/MKSettingsProvider/src/org/mokee/mksettings/MKDatabaseHelper.java
@@ -260,6 +260,9 @@ public class MKDatabaseHelper extends SQLiteOpenHelper{
         loadBooleanSetting(db, MKTableNames.TABLE_SYSTEM, MKSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE,
                 R.bool.def_notification_pulse_custom_enable);
 
+        loadBooleanSetting(db, MKTableNames.TABLE_SYSTEM, MKSettings.System.SWAP_VOLUME_KEYS_ON_ROTATION,
+                R.bool.def_swap_volume_keys_on_rotation);
+
         if (mContext.getResources().getBoolean(R.bool.def_notification_pulse_custom_enable)) {
             loadStringSetting(db, MKTableNames.TABLE_SYSTEM, MKSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES,
                     R.string.def_notification_pulse_custom_value);
