ed0650deaf760c86bb9fcd35db3c53982a121151
Keith Mok <kmok@cyngn.com>
2016-02-04 10:49:03 -0800

    mksdk : Add caffeine tile (2/2)
    Change-Id: I6c3d5cdc0dc2cce06f1c00c6ffbb96d5f376dc46

--- src/java/org/mokee/internal/logging/MKMetricsLogger.java
+++ src/java/org/mokee/internal/logging/MKMetricsLogger.java
@@ -65,4 +65,5 @@ public class MKMetricsLogger extends MetricsLogger {
     public static final int TILE_VOLUME = BASE + 38;
     public static final int TILE_HEADS_UP = BASE + 39;
     public static final int TILE_BATTERY_SAVER = BASE + 40;
+    public static final int TILE_CAFFEINE = BASE + 41;
 }
