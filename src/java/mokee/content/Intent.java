b14dfa86e7aed66e91c9301ce6f22276cf4d8583
Roman Birg <roman@cyngn.com>
2016-03-25 10:13:45 -0700

    mksdk: add live lock screen settings action intent
    Ticket: CYNGNOS-2241
    Change-Id: I42dbfc96807d1f2aad1f84b100b5e7a2ab68daea
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/java/mokee/content/Intent.java
+++ src/java/mokee/content/Intent.java
@@ -135,4 +135,11 @@ public class Intent {
      */
     public static final String URI_SCHEME_PACKAGE = "package";
 
+    /**
+     * Implicit action to open live lock screen settings.
+     * @hide
+     */
+    public static final String ACTION_OPEN_LIVE_LOCKSCREEN_SETTINGS =
+            "mokee.intent.action.OPEN_LIVE_LOCKSCREEN_SETTINGS";
+
 }
