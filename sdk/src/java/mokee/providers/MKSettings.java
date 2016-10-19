1f9b5ba3f6a13683d4282dff38de1d6d32639373
Steve Kondik <steve@cyngn.com>
2016-10-18 23:37:02 -0700

    mksdk: Add support for split screen button behavior
    Change-Id: I7289e8abe23d047f909066a9986e8b9d55bec27e

--- sdk/src/java/mokee/providers/MKSettings.java
+++ sdk/src/java/mokee/providers/MKSettings.java
@@ -1046,12 +1046,13 @@ public final class MKSettings {
          * 6 - Launch Camera
          * 7 - Action Sleep
          * 8 - Last app
+         * 9 - Toggle split screen
          */
         public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
 
         /** @hide */
         public static final Validator KEY_HOME_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the home key is double-tapped.
@@ -1062,7 +1063,7 @@ public final class MKSettings {
 
         /** @hide */
         public static final Validator KEY_HOME_DOUBLE_TAP_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Whether to wake the screen with the back key, the value is boolean.
@@ -1102,7 +1103,7 @@ public final class MKSettings {
 
         /** @hide */
         public static final Validator KEY_MENU_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the menu key is long-pressed.
@@ -1113,7 +1114,7 @@ public final class MKSettings {
 
         /** @hide */
         public static final Validator KEY_MENU_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the assistant (search) key is pressed. (Default is 3)
@@ -1123,7 +1124,7 @@ public final class MKSettings {
 
         /** @hide */
         public static final Validator KEY_ASSIST_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the assistant (search) key is long-pressed. (Default is 4)
@@ -1133,7 +1134,7 @@ public final class MKSettings {
 
         /** @hide */
         public static final Validator KEY_ASSIST_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the app switch key is pressed. (Default is 2)
@@ -1143,7 +1144,7 @@ public final class MKSettings {
 
         /** @hide */
         public static final Validator KEY_APP_SWITCH_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Action to perform when the app switch key is long-pressed. (Default is 0)
@@ -1153,7 +1154,7 @@ public final class MKSettings {
 
         /** @hide */
         public static final Validator KEY_APP_SWITCH_LONG_PRESS_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 8);
+                new InclusiveIntegerRangeValidator(0, 9);
 
         /**
          * Whether to wake the screen with the home key, the value is boolean.
