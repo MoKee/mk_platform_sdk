48a4d0e70aa4e96fd4f8f5cd3504e8fcdb9831cc
Gabriele M <moto.falcon.git@gmail.com>
2016-11-08 14:00:29 +0100

    PerformanceManager: Fix power hint values
    Sync them with hardware/libhardware/include/hardware/power.h
    
    Change-Id: Id9d454df40625b6fd743dbbb3eb8d10489c7e377

--- mk/lib/main/java/org/mokee/platform/internal/PerformanceManagerService.java
+++ mk/lib/main/java/org/mokee/platform/internal/PerformanceManagerService.java
@@ -83,9 +83,9 @@ public class PerformanceManagerService extends MKSystemService {
     private final BoostHandler mHandler;
 
     // keep in sync with hardware/libhardware/include/hardware/power.h
-    private final int POWER_HINT_CPU_BOOST    = 0x00000010;
-    private final int POWER_HINT_LAUNCH_BOOST = 0x00000011;
-    private final int POWER_HINT_SET_PROFILE  = 0x00000030;
+    private final int POWER_HINT_CPU_BOOST    = 0x00000110;
+    private final int POWER_HINT_LAUNCH_BOOST = 0x00000111;
+    private final int POWER_HINT_SET_PROFILE  = 0x00000113;
 
     private final int POWER_FEATURE_SUPPORTED_PROFILES = 0x00001000;
 
